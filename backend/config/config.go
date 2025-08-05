649644dc6bc428978838ace77719487690c7938f
torbenconto <torbenmconto@gmail.com>
2025-08-04 20:33:10 -0400

    viper bindings

--- backend/config/config.go
+++ backend/config/config.go
@@ -60,6 +60,33 @@ func LoadConfig(path string) (*Config, error) {
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
+	_ = viper.BindEnv("port")
+	_ = viper.BindEnv("secret_key")
+	_ = viper.BindEnv("mode")
+	_ = viper.BindEnv("email_whitelist_enabled")
+	_ = viper.BindEnv("cors_allow_origins")
+
+	_ = viper.BindEnv("features.email_whitelist_enabled")
+
+	_ = viper.BindEnv("supabase.host")
+	_ = viper.BindEnv("supabase.port")
+	_ = viper.BindEnv("supabase.user")
+	_ = viper.BindEnv("supabase.password")
+	_ = viper.BindEnv("supabase.database")
+
+	_ = viper.BindEnv("smtp.host")
+	_ = viper.BindEnv("smtp.port")
+	_ = viper.BindEnv("smtp.email")
+	_ = viper.BindEnv("smtp.password")
+
+	_ = viper.BindEnv("admin.email")
+	_ = viper.BindEnv("admin.first_name")
+	_ = viper.BindEnv("admin.last_name")
+
+	_ = viper.BindEnv("storage.provider")
+	_ = viper.BindEnv("storage.google_cloud.bucket_name")
+	_ = viper.BindEnv("storage.local.base_path")
+
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
 			return nil, err
